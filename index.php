196f5d51af5b47abb07cf37846ecb67076586164
Roze <rozeu668@gmail.com>
2024-04-30 11:33:22 +0300

    Uzsāka projekta versionēšanu

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="lv">
+
+
+<head>
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+</head>
+
+
+<body>
+
+    <header>
+        
+        <a href="#" class="logo">IT ir Spēks</a>
+        
+        <nav>
+
+            <a href="#">Sākumlapa</a>
+            <a href="#jaunumi">Jaunumi</a>
+            <a href="#vakances">Vakances</a>
+            <a href="#Par_mums">Par mums</a>
+            <a href="#Kontaktēties">Kontaktēties</a>
+        </nav>
+
+        <div id="menu-bar" class="fas fa-bars"></div>   <!-- Priekš viedierīcēm, kad ekrāns too small :)-->
+
+    </header>
+
+
+
+
+
+
+
+
+
+    
+    <footer>
+
+
+    Vidoja only viljams :C
+
+
+    </footer>
+
+</body>
+
+
+</html>
