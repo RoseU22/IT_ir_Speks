189ce6042283f2a20dfde5d22870974fb68cfd91
RozeU <rozeu668@gmail.com>
2024-05-21 23:13:51 +0300

    Tika pabeigta mājaslapas Vakances sadaļa, kur tika pievienota jauna mājaslapas lapa, kurā var apskatīt vairākas pieejamās vakances, kuras netiek rādītās mājaslapas sākumlapā

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="lv">
 <head>
 
     <meta charset="UTF-8">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- <link rel="stylesheet" href='assets/styles/background.css'> -->
     <link rel="stylesheet" href='assets/styles/main.css'>
+    <link rel="stylesheet" href='assets/styles/footer.css'>
 
     <link rel="icon" type="image/x-icon" href="assets/images/logo.png">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
     <title>IT ir Spēks</title>
 
@@ -24,8 +26,8 @@
         <nav>
             <a href="#">Sākums</a>
             <a href="#jaunumi">Jaunumi</a>
-            <a href="">Vakances</a>
-            <a href="">Par mums</a>
+            <a href="#vakances">Vakances</a>
+            <a href="#parmums">Par mums</a>
             <a href="login.php">Autorizācija darbiniekiem</a>
         </nav>
 
@@ -39,7 +41,7 @@
 
             <!--Salabot pogu lai būtu responsīva-->
 
-            <button onclick="location.href='#jaunumi'" type="button" class="hover-button">Pieejamās vakances!</button>
+            <button onclick="location.href='#vakances'" type="button" class="hover-button">Pieejamās vakances!</button>
 
         </div>
 
@@ -82,6 +84,115 @@
         <script src="assets/scripts/jaunumisadala.js"></script>
     
     </section>
+
+    <section id="vakances">
+
+        <h1>Pieejamās <span>vakances!</span></h1>
+
+        <div class="box-container">
+
+            <div class="box">
+
+                <img src="assets/images/placeholder.jpeg" alt="">
+                <h3>Vakance 1</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam ipsa nihil. Excepturi ab omnis sed veritatis soluta minus iste. Quidem quas officiis doloribus, pariatur expedita reiciendis distinctio nulla ullam!</p>
+
+            </div>
+
+            <div class="box">
+
+                <img src="assets/images/placeholder.jpeg" alt="">
+                <h3>Vakance 2</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus quam accusamus provident aperiam ipsam voluptatum est culpa, quasi officia sunt molestiae quibusdam temporibus asperiores reprehenderit sapiente fugiat amet a quos!</p>
+
+            </div>
+
+            <div class="box">
+
+                <img src="assets/images/placeholder.jpeg" alt="">
+                <h3>Vakance 3</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae repellat voluptas numquam eveniet culpa harum commodi ipsam pariatur incidunt cum aspernatur, fuga dolorum aliquam praesentium reprehenderit. Temporibus incidunt error ratione?</p>
+
+            </div>
+
+        </div>
+
+        <div class="kreep">
+            <button onclick="location.href='vakances.php'">Parādīt vēl!</button>
+        </div>   
+
+    </section>
+
+    <section id="parmums">
+
+        <div class="informacija">
+
+            <h1>Par <span>Mums!</span></h1>
+
+            <p>Mēs sniedzam jaunas iespējas katram cilvēkam, piedāvājot vakances, kuras ir atrodamas apkārt visai Latvijai. Mūsu mērķis ir palīdzēt cilvēkiem atrast brīvas vakances, kuras tiek piedāvātas šeit priekš ērtas meklēšanas un atrašanas.</p>
+
+        </div>
+
+        <img src="assets/images/lookingforjob.png" alt="">
+
+        <div class="kontakti">
+
+            <h1>Kontakti</h1>
+
+            <div class="box-container">
+
+                <div class="icon">
+
+                    <i class="fas fa-phone"></i>
+
+                    <h3>Tālrunis</h3>
+
+                    <p>+371 25 175 286</p>
+                    
+                </div>
+
+                <div class="icon">
+
+                    <i class="fas fa-envelope"></i>
+
+                    <h3>E-pasts</h3>
+
+                    <p>ITirSpeks@ITirSpeks.lv</p>
+                    
+                </div>
+
+                <div class="icon">
+
+                    <i class="fas fa-map-marker-alt"></i>
+
+                    <h3>Adrese</h3>
+
+                    <p>Ventspils iela 51, <br> Liepāja, LV-3405, Latvija</p>
+                    
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <footer>
+
+        <div class="loop-wrapper">
+            <div class="mountain"></div>
+            <div class="hill"></div>
+            <div class="tree"></div>
+            <div class="tree"></div>
+            <div class="tree"></div>
+            <div class="rock"></div>
+            <div class="truck"></div>
+            <div class="wheels"></div>
+        </div> 
+        
+        <p>IT ir Spēks © 2024</p>
+    
+    </footer>
     
 </body>
 </html>
---
+++ vakances.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="lv">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vakances</title>
+
+    <link rel="stylesheet" href='assets/styles/main.css'>
+    <link rel="stylesheet" href='assets/styles/footer.css'>
+    <link rel="stylesheet" href='assets/styles/vakances.css'>
+
+    <link rel="icon" type="image/x-icon" href="assets/images/logo.png">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <title>IT ir Spēks</title>
+
+</head>
+
+<body>
+
+    <header>
+
+        <a href="#" class="logo">IT ir Spēks</a>
+
+        <nav>
+            <a href="index.php#">Sākums</a>
+            <a href="index.php#jaunumi">Jaunumi</a>
+            <a href="index.php#vakances">Vakances</a>
+            <a href="index.php#parmums">Par mums</a>
+            <a href="login.php">Autorizācija darbiniekiem</a>
+
+            <div class="meklet">
+
+                <form action="#">
+                    
+                    <input type="text" placeholder="Meklēt vakances" name="meklet">
+                    <button><i class="fa fa-search" style="font-size: 18px;"></i></button>
+
+                </form>
+
+            </div>
+
+        </nav>
+
+    </header>
+
+    <div class="visasvakances">
+
+        <table class="vakancesKaste">
+
+            <tbody>
+
+                <tr>
+
+                    <td><img src="assets/images/pavars.jpg" alt=""></td>
+                    <td><h1>Pavārs</h1><p>Darba pienākums ir ēdienu gatavošana atbilstoši ēdienkartei, kā arī pozitīva attieksme pret veicamo darbu un ievērot sanitārās un higiēnas normas.</p><button>Pieteikties</button></td>
+
+                </tr>
+
+                <tr>
+
+                    <td><img src="assets/images/barmenis.jpg" alt=""></td>
+                    <td><h1>Bārmenis</h1><p>Darbs bārā restorānos Rīgā. Darba pienākumi ir viesmīlīga un profesionāla viesu apkalpošana un bāra produkcijas profesionāla pagatavošana pēc noteiktajām kalkulācijām, ievērojot sanitārās un higiēnas normas.</p><button>Pieteikties</button></td>
+
+                </tr>
+
+                <tr>
+
+                    <td><img src="assets/images/ekonomists.jpg" alt=""></td>
+                    <td><h1>Ekonomists</h1><p>Darba pienākumi ir apkopot pašvaldības institūciju iesniegtos budžeta pieprasījumus, veikt to pārbaudi un analīzi, Nodrošināt pašvaldības institūciju apstiprinātās tāmes izpildes kontroli ekonomiskās klasifikācijas un funkcionālo kategoriju klasifikācijas kodu griezumā,Analizēt budžeta rādītājus, kā arī līdzekļu izlietojumu atbilstoši piešķirtajam asignējumiem un apstiprinātajām budžeta tāmēm pa funkcionālajām kategorijām un ekonomiskās klasifikācijas kodiem u.c.</p><button>Pieteikties</button></td>
+
+                </tr>
+
+                <tr>
+
+                    <td><img src="assets/images/gramatvedis.png" alt=""></td>
+                    <td><h1>Grāmatvedis</h1><p>Darba pienākumi ir izstrādāt un ieviest finanšu vadības un grāmatvedības politiku, Optimizēt grāmatvedības procesus un pilnveidot grāmatvedības sistēmu, Organizēt pilna cikla grāmatvedības uzskaites procesu atbilstoši Latvijas Republikas likumdošanas un normatīvo aktu prasībām, Nodrošināt naudas līdzekļu, materiālo vērtību un pamatlīdzekļu uzskaiti, Sagatavot finanšu un nodokļu atskaites vadībai un valsts institūcijām un sagatavot gada pārskatus.</p><button>Pieteikties</button></td>
+
+                </tr>
+
+                <tr>
+
+                    <td><img src="assets/images/sistemanalitikis.jpg" alt=""></td>
+                    <td><h1>Sistēmanalītiķis</h1><p>Darba pienākumi ir pētīt, analizēt un izvērtēt tiesībsargājošo iestāžu darba procesus, izstrādāt prasības IT risinājumiem kriminātiesiskajā jomā, piedalīties šo risinājuma izpētē, attīstīšanā un ieviešanā un organizēt un nodrošināt neformālās izglītības pasākumu īstenošanu tiesībaizsardzības iestāžu amatpersonām IT jomā.</p><button>Pieteikties</button></td>
+
+                </tr>
+
+            </tbody>
+
+        </table>
+
+    </div>
+
+    <footer>
+
+        <div class="loop-wrapper">
+            <div class="mountain"></div>
+            <div class="hill"></div>
+            <div class="tree"></div>
+            <div class="tree"></div>
+            <div class="tree"></div>
+            <div class="rock"></div>
+            <div class="truck"></div>
+            <div class="wheels"></div>
+        </div> 
+
+        <p>IT ir Spēks © 2024</p>
+
+    </footer>
+    
+</body>
+
+
+</html>
